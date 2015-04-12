e20c7242ed1e6dc61345c977a81e618b344a2fab
Jay Bisa <jaybisa@Jay-Bisas-MBP.home>
2015-04-12 17:40:03 -0400

    Show standings in AL East.

---
+++ standings.js
@@ -0,0 +1,107 @@
+"use strict";
+var https = require('https');
+var fs = require('fs');
+var moment = require('moment-timezone');
+var sprintf = require('sprintf').sprintf;
+var zlib = require('zlib');
+
+// Replace with your access token
+var ACCESS_TOKEN = '4b22e306-f608-43f5-8f96-d4dcb7e08733';
+
+// Replace with your bot name and email/website to contact if there is a problem
+// e.g., "mybot/0.1 (https://erikberg.com/)"
+var USER_AGENT = 'ALEastBot (aleastbot@gmail.com)';
+
+// Set time zone to use for output
+var TIME_ZONE = 'America/New_York';
+
+function main() {
+    // Set the API method, format, and any parameters
+    var host   = 'erikberg.com';
+    var sport  = 'mlb';
+    var method = 'standings';
+    var id     = '20150412';
+    var format = 'json';
+
+    var url;
+    var default_opts;
+    var chunks;
+    var buffer;
+    var encoding;
+
+    url = buildURL(host, sport, method, id, format);
+
+    default_opts = {
+        'host': host,
+        'path': url,
+        'headers': {
+            'Accept-Encoding': 'gzip',
+            'Authorization': 'Bearer ' + ACCESS_TOKEN,
+            'User-Agent': USER_AGENT
+        }
+    };
+
+    https.get(default_opts, function (res) {
+        chunks = [];
+        res.on('data', function (chunk) {
+            chunks.push(chunk);
+        });
+        res.on('end', function () {
+            if (res.statusCode !== 200) {
+                // handle error...
+                console.warn("Server did not return a 200 response!\n" + chunks.join(''));
+                process.exit(1);
+            }
+            encoding = res.headers['content-encoding'];
+            if (encoding === 'gzip') {
+                buffer = Buffer.concat(chunks);
+                zlib.gunzip(buffer, function (err, decoded) {
+                    if (err) {
+                        console.warn("Error trying to decompress data: " + err.message);
+                        process.exit(1);
+                    }
+                    printResults(decoded);
+                });
+            } else {
+                printResults(chunks.join(''));
+            }
+        });
+    }).on('error', function (err) {
+        console.warn("Error trying to contact server: " + err.message);
+        process.exit(1);
+    });
+}
+
+// See https://erikberg.com/api/methods Request URL Convention for
+// an explanation
+function buildURL(host, sport, method, id, format) {
+    var ary = [sport, method, id];
+    var path;
+    var url;
+
+    path = ary.filter(function (element) {
+        return element !== undefined;
+    }).join('/');
+    url = 'https://' + host + '/' + path + '.' + format;
+
+    return url;
+}
+
+function printResults(content) {
+    var standings = JSON.parse(content);
+
+    standings.standing.forEach(function (standing) {
+        if (standing.conference == "AL" && standing.division == "E") {
+            /*
+            process.stdout.write(sprintf("%12s %12s %-12s\n",
+                standing.ordinal_rank,
+                standing.first_name,
+                standing.last_name));
+             */
+
+            console.log(standing.ordinal_rank + " " + standing.first_name + " " + standing.last_name);
+        }
+    });
+}
+
+main();
